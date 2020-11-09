bdeb823c3a8d395a553bb085b19e32fffa865025
Vebbyebih <vebbyebih@gmail.com>
2020-11-09 16:30:36 +0700

    Body email pajak

---
+++ mail/pajak-penghapusan.php
@@ -0,0 +1,35 @@
+<html>
+
+<h1> Pemberitahuan Pengingat Penghapusan Kendaraan </h1>
+
+<body>
+
+Kepala Subbagian Rumah Tangga yang terhormat,
+
+Kami mengingatkan bahwa anda perlu untuk penghapusan kendaraan karna kendaraan telah lebih dari 7 tahun pemakaian. dengan spesifikasi Kendaraan sebagai berikut :
+
+Merk Kendaraan :
+
+Tipe Kendaraan :
+
+No Plat Hitam :
+
+No Plat Merah :
+
+No STNK :
+
+Unit Pemegang Kendaraan :
+
+Pemakaian Tahun ke :
+
+
+Salam
+
+Direktorat Jenderal Perhubungan Laut
+
+--------------------------------------------------------------
+Email ini dikirim secara otomatis oleh sistem. Anda tidak perlu membalas atau mengirim email ke alamat ini.
+
+</body>
+
+</html>
